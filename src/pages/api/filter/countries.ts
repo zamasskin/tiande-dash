d770b708db2dd5a7605cc9b19ef5aad14b9a8676
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-25 10:45:12 +0300

    Получение доступных для пользователя стран

--- src/pages/api/filter/countries.ts
+++ src/pages/api/filter/countries.ts
@@ -2,12 +2,25 @@ import type { NextApiHandler } from "next";
 import { qb } from "../../../settings/api";
 
 const countriesHandler: NextApiHandler = async (request, response) => {
-  const { amount = 1 } = request.body;
+  const { useId = 0, allowed = true } = request.body;
 
-  const countries = await qb("bit_country").select(
+  let countries = await qb("bit_country").select(
     "ID as value",
     "UF_NAME as text"
   );
+
+  if (!allowed) {
+    const properties = await qb("b_utm_user")
+      .where("FIELD_ID", "2011")
+      .where("VALUE_ID", useId)
+      .select("VALUE_INT", "id");
+
+    const allowedCountries = properties.map((p) => Number(p.id));
+    countries = countries.filter((c) =>
+      allowedCountries.includes(Number(c.value))
+    );
+  }
+
   response.json({ result: "ok", data: countries });
 };
 
