43178a47425be2000555089ba1dce53aae9c45ba
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-25 10:36:08 +0300

    Поставил и настроил knex

---
+++ src/settings/api.ts
@@ -0,0 +1,16 @@
+import knex from "knex";
+
+export const qb = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DATABASE_HOST || "127.0.0.1",
+    port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    pool: {
+      min: parseInt(process.env.DATABASE_MIN_CONNECTIONS, 10) || 0,
+      max: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10) || 12,
+    },
+  },
+});
