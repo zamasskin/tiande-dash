ed3575a07c0868470740cbe5f95ba53b44d2a29d
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-29 14:08:00 +0300

    Не нужен

--- src/features/components/DateRangePicker.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { polyfill } from "react-lifecycles-compat";
-import { DateRangePicker } from "react-dates";
-// polyfill(DateRangePicker);
-export default DateRangePicker;
