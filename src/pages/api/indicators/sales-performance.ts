03257758da1ab80cbf25400d6fbe5645b1590b11
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-08 17:19:20 +0300

    Использовал не тот метод

--- src/pages/api/indicators/sales-performance.ts
+++ src/pages/api/indicators/sales-performance.ts
@@ -1,9 +1,9 @@
 import type { NextApiHandler } from "next";
-import { SalesPerformanceQuery } from "../../../models/indicators";
+import { SalesPerformance } from "../../../models/indicators";
 
 const salesPerformance: NextApiHandler = async (request, response) => {
   const { filter } = request.body;
-  const result = await SalesPerformanceQuery(filter);
+  const result = await SalesPerformance(filter);
   response.json({ result: "ok", data: result });
 };
 
