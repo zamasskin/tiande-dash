f92a95c8aea04eb93ecf2dcac06d4e9cea4afa4f
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-08 19:09:08 +0300

    Проверка на подключение таблиц

--- src/models/indicators/index.ts
+++ src/models/indicators/index.ts
@@ -14,70 +14,104 @@ export function indicatorsQuery() {
     .where("o.DELIVERY_ID", "<>", "");
 }
 
-export function currencyRateJoin(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ k: "b_catalog_currency_rate" }, (qb) =>
-    qb
-      .on("k.DATE_RATE", knex.raw("date_format(o.DATE_INSERT, '%Y-%m-%d')"))
-      .andOn("k.CURRENCY", "o.CURRENCY")
+export function checkRequiredJoin(
+  query,
+  searchKey: string,
+  searchValue: string
+) {
+  const joins = query._statements.filter(
+    (obj) => "joinType" in obj && "table" in obj
   );
+  return !!joins.find(
+    (obj) =>
+      Object.keys(obj.table).join("") === searchKey &&
+      Object.values(obj.table).join("") === searchValue
+  );
+}
+
+//query._statements
+export function currencyRateJoin(qb: Knex.QueryBuilder) {
+  return checkRequiredJoin(qb, "k", "b_catalog_currency_rate")
+    ? qb
+    : qb.leftJoin({ k: "b_catalog_currency_rate" }, (qb) =>
+        qb
+          .on("k.DATE_RATE", knex.raw("date_format(o.DATE_INSERT, '%Y-%m-%d')"))
+          .andOn("k.CURRENCY", "o.CURRENCY")
+      );
 }
 
 export function joinLocationProps(qb: Knex.QueryBuilder) {
-  return qb.join({ op: "b_sale_order_props_value" }, (qb) =>
-    qb.on("op.ORDER_ID", "o.ID").andOn("op.ORDER_PROPS_ID", knex.raw(2))
-  );
+  return checkRequiredJoin(qb, "op", "b_sale_order_props_value")
+    ? qb
+    : qb.join({ op: "b_sale_order_props_value" }, (qb) =>
+        qb.on("op.ORDER_ID", "o.ID").andOn("op.ORDER_PROPS_ID", knex.raw(2))
+      );
 }
 
 export function joinStorageProps(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ op7: "b_sale_order_props_value" }, (qb) =>
-    qb.on(" op7.ORDER_ID", "o.ID").andOn("op7.ORDER_PROPS_ID", knex.raw(43))
-  );
+  return checkRequiredJoin(qb, "op7", "b_sale_order_props_value")
+    ? qb
+    : qb.leftJoin({ op7: "b_sale_order_props_value" }, (qb) =>
+        qb.on(" op7.ORDER_ID", "o.ID").andOn("op7.ORDER_PROPS_ID", knex.raw(43))
+      );
 }
 
 export function joinLoyaltyProps(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ op3: "b_sale_order_props_value" }, (qb) =>
-    qb.on("op3.ORDER_ID", "o.ID").andOn("op3.ORDER_PROPS_ID", knex.raw(126))
-  );
+  return checkRequiredJoin(qb, "op3", "b_sale_order_props_value")
+    ? qb
+    : qb.leftJoin({ op3: "b_sale_order_props_value" }, (qb) =>
+        qb.on("op3.ORDER_ID", "o.ID").andOn("op3.ORDER_PROPS_ID", knex.raw(126))
+      );
 }
 
 export function joinLocation(qb: Knex.QueryBuilder) {
-  return joinLocationProps(qb).join(
-    { l: "b_sale_location" },
-    "l.ID",
-    "op.VALUE"
-  );
+  return checkRequiredJoin(qb, "l", "b_sale_location")
+    ? joinLocationProps(qb)
+    : joinLocationProps(qb).join({ l: "b_sale_location" }, "l.ID", "op.VALUE");
 }
 
 export function joinBitCountry(qb: Knex.QueryBuilder) {
-  return joinLocation(qb).join(
-    { c: "bit_country" },
-    "c.UF_BITRIX_COUNTRY",
-    "l.COUNTRY_ID"
-  );
+  return checkRequiredJoin(qb, "c", "bit_country")
+    ? joinLocation(qb)
+    : joinLocation(qb).join(
+        { c: "bit_country" },
+        "c.UF_BITRIX_COUNTRY",
+        "l.COUNTRY_ID"
+      );
 }
 
 export function joinIsAppProps(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ op4: "b_sale_order_props_value" }, (qb) =>
-    qb.on("op4.ORDER_ID", "o.ID").andOn("op4.ORDER_PROPS_ID", knex.raw(92))
-  );
+  return checkRequiredJoin(qb, "op4", "b_sale_order_props_value")
+    ? qb
+    : qb.leftJoin({ op4: "b_sale_order_props_value" }, (qb) =>
+        qb.on("op4.ORDER_ID", "o.ID").andOn("op4.ORDER_PROPS_ID", knex.raw(92))
+      );
 }
 
 export function joinPickupProps(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ op6: "b_sale_order_props_value" }, (qb) =>
-    qb.on("op6.ORDER_ID", "o.ID").andOn("op6.ORDER_PROPS_ID", knex.raw(69))
-  );
+  return checkRequiredJoin(qb, "op6", "b_sale_order_props_value")
+    ? qb
+    : qb.leftJoin({ op6: "b_sale_order_props_value" }, (qb) =>
+        qb.on("op6.ORDER_ID", "o.ID").andOn("op6.ORDER_PROPS_ID", knex.raw(69))
+      );
 }
 
 export function joinIsBoutiqueProps(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ op8: "b_sale_order_props_value" }, (qb) =>
-    qb.on("op8.ORDER_ID", "o.ID").andOn("op8.ORDER_PROPS_ID", knex.raw(127))
-  );
+  return checkRequiredJoin(qb, "op8", "b_sale_order_props_value")
+    ? qb
+    : qb.leftJoin({ op8: "b_sale_order_props_value" }, (qb) =>
+        qb.on("op8.ORDER_ID", "o.ID").andOn("op8.ORDER_PROPS_ID", knex.raw(127))
+      );
 }
 
 export function joinUser(qb: Knex.QueryBuilder) {
-  return qb.leftJoin({ u: "b_user" }, "u.ID", "o.USER_ID");
+  return checkRequiredJoin(qb, "u", "b_user")
+    ? qb
+    : qb.leftJoin({ u: "b_user" }, "u.ID", "o.USER_ID");
 }
 
 export function joinUserProperties(qb: Knex.QueryBuilder) {
-  return joinUser(qb).leftJoin({ ut: "b_uts_user" }, "ut.VALUE_ID", "u.ID");
+  return checkRequiredJoin(qb, "ut", "b_uts_user")
+    ? joinUser(qb)
+    : joinUser(qb).leftJoin({ ut: "b_uts_user" }, "ut.VALUE_ID", "u.ID");
 }
