c09c8d48dd73ae6450e7ec76e6b585d0bec8be68
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-11 16:06:35 +0300

    Запрос для  планфакт продаж

---
+++ src/models/indicators/planFactAnalysis.ts
@@ -0,0 +1,46 @@
+import _ from "lodash";
+
+import { qb as knex } from "../../settings/api";
+import { indicatorsQuery, currencyRateJoin, joinLocationProps } from ".";
+import { FilterState } from "../../features/indicators/filterSlice";
+import { prepareFilterPlanFactIndicators } from "./filter";
+import { startTime } from "../../features/functions/date";
+import { numberFormatRub } from "../number";
+
+export function planQuery(filter: FilterState) {
+  let query = indicatorsQuery();
+  query = currencyRateJoin(query);
+  query = prepareFilterPlanFactIndicators(query, filter);
+  query = joinLocationProps(query);
+  query = query.select(
+    knex.raw(
+      "round(sum(if(o.CURRENCY='RUB', o.price, o.price / k.RATE_CNT * k.RATE)),2) as 'fact'"
+    )
+  );
+  return query.first();
+}
+
+export function factQuery(filter: FilterState) {
+  const periodStart = startTime(filter.periodStart);
+  let query = knex("plan_order")
+    .select("UF_VALUE as plan")
+    .where("UF_PERIOD", new Date(periodStart));
+  if (filter.country > 0) {
+    query = query.where("UF_COUNTRY", filter.country);
+  }
+  return query.first();
+}
+
+export async function planFactAnalysis(filter: FilterState) {
+  const [{ fact }, { plan }] = await Promise.all([
+    planQuery(filter),
+    factQuery(filter),
+  ]);
+
+  return {
+    fact: numberFormatRub(fact),
+    plan: numberFormatRub(fact),
+    abc: numberFormatRub(plan - fact),
+    otn: numberFormatRub(100 - _.round(((plan - fact) / plan) * 100, 2)),
+  };
+}
