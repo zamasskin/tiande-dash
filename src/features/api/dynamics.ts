fb227fd8e205e2c39e5138ca1e3ba94dba9570d4
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-12 15:28:48 +0300

    Переименовал

--- src/features/api/dynamics.ts
+++ src/features/api/dynamics.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { DynamicsFilterState } from "../dynamics/filterSlice";
 import { prepareResponse, Response } from "./index";
 
-export async function fetchDeliveryGroupList(filter: DynamicsFilterState) {
+export async function fetchDynamicSaleList(filter: DynamicsFilterState) {
   const response = await axios.post<Response>("/api/dynamics/dynamic-sale", {
     filter,
   });
