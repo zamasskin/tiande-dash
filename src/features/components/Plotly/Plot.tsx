689188b3d439afb8db7778443bea3b11db133f9c
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-12 16:38:28 +0300

    Обернул тк не совместим с nestjs

---
+++ src/features/components/Plotly/Plot.tsx
@@ -0,0 +1,25 @@
+import dynamic from 'next/dynamic'
+import * as csDictionary from 'plotly.js/lib/locales/ru.js';
+
+interface PlotProperty {
+  data?: any, 
+  layout?: any, 
+  style?: any, 
+  config?:any
+}
+
+function Plot(properties: PlotProperty) {
+  const config = {
+    ...(properties.config || {}),
+    locales: { 'ru': csDictionary },
+    locale: 'ru'
+  } 
+  const DynamicComponentWithNoSSR = dynamic<PlotProperty>(import('react-plotly.js'), {
+    ssr: false
+  })
+  return (
+    <DynamicComponentWithNoSSR {...properties} config={config} />
+  )
+}
+
+export default Plot
