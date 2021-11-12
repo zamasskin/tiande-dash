b20821c921bc29453a01bdeebf699925c204fda4
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-12 12:13:09 +0300

    Исправил запросы

--- src/models/indicators/delivery.ts
+++ src/models/indicators/delivery.ts
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import { unserialize } from "php-serialize";
 
 import { qb as knex } from "../../settings/api";
 
 import { FilterState } from "../../features/indicators/filterSlice";
-import { indicatorsQuery, currencyRateJoin, joinLocationProps } from ".";
+import {
+  indicatorsQuery,
+  currencyRateJoin,
+  joinLocationProps,
+  joinPickupProps,
+  joinPvzProps,
+} from ".";
 import { prepareFilter } from "./filter";
 import { numberFormatRub } from "../number";
 
@@ -11,8 +18,10 @@ export function deliveryQuery(filter: FilterState) {
   let query = indicatorsQuery();
   query = currencyRateJoin(query);
   query = joinLocationProps(query);
+  query = joinPickupProps(query);
+  query = joinPvzProps(query);
   query = prepareFilter(query, filter);
-
+  query.groupBy("o.DELIVERY_ID", "op6.VALUE", "op9.VALUE");
   query = query.select(
     "o.DELIVERY_ID as name",
     knex.raw(
@@ -28,24 +37,60 @@ export function deliveryQuery(filter: FilterState) {
       END type
     `)
   );
+  query = knex(query.as("t"));
   query = query.select("*", knex.raw("SUM(salesSum) AS sum"));
   return query;
 }
 
-export function prepareDeliveryList(deliveryIndicators: any[]) {
-  const $sum = _.sumBy(deliveryIndicators, "sum");
-  deliveryIndicators.map((indicators) => ({
+export async function staticDeliveryList() {
+  const deliveryList = await knex("b_sale_delivery").select(
+    "ID as id",
+    "NAME as name"
+  );
+  return _.mapValues(_.keyBy(deliveryList, "id"), "name");
+}
+
+export async function dynamicDeliveryList() {
+  let deliveryList = await knex("b_sale_delivery_handler").select(
+    "HID as hid",
+    "NAME as name",
+    "PROFILES as profiles"
+  );
+  deliveryList = deliveryList.map((delivery) => {
+    const profiles = unserialize(delivery.profiles);
+    const profileName = _.first(Object.keys(profiles));
+    return {
+      id: [delivery.hid, profileName].join(":"),
+      ...delivery,
+    };
+  });
+  return _.mapValues(_.keyBy(deliveryList, "id"), "name");
+}
+
+export async function prepareDeliveryList(deliveryIndicators: any[]) {
+  const [staticList, dynamicList] = await Promise.all([
+    staticDeliveryList(),
+    dynamicDeliveryList(),
+  ]);
+  deliveryIndicators = deliveryIndicators.map((indicators) => ({
     ...indicators,
+    sum: Number(indicators.sum) || 0,
+  }));
+  const $sum = _.sumBy(deliveryIndicators, "sum");
+  return deliveryIndicators.map((indicators) => ({
+    ...indicators,
+    name:
+      staticList[indicators.name] ||
+      dynamicList[indicators.name] ||
+      indicators.name,
     salesSum: numberFormatRub(indicators.salesSum),
     sum: (indicators.sum / $sum) * 100,
   }));
 }
 
 export async function deliveryList(filter: FilterState) {
-  const deliveryIndicators = await deliveryQuery(filter).groupBy(
-    "name",
-    "type"
-  );
+  const query = deliveryQuery(filter).groupBy("name", "type");
+  const deliveryIndicators = await query;
   return prepareDeliveryList(deliveryIndicators);
 }
 
