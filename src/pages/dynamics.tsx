fe8a41d1457c9e92c26741427df8879ec2e017a6
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-16 16:25:17 +0300

    Подключил авторизацию

--- src/pages/dynamics.tsx
+++ src/pages/dynamics.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from 'next'
 import Router from 'next/router'
 import { useEffect } from "react";
 import Head from 'next/head'
-import { Container, Navbar, Row, Col} from 'react-bootstrap'
+import { Container, Navbar, Row, Col, Card, Spinner} from 'react-bootstrap'
 
 import Filter from "../features/dynamics/Filter"
 import DynamicSale from "../features/dynamics/DynamicSale"
+import Login from '../features/components/Login'
 
 const DynamicsPage: NextPage = () => {
   return <>
@@ -13,31 +14,27 @@ const DynamicsPage: NextPage = () => {
       <title>Динамика продаж</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Navbar bg="primary" variant="dark" className="mb-3">
+    <Login>
+      <Navbar bg="primary" variant="dark" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="/dynamics">Отчеты</Navbar.Brand>
+        </Container>
+      </Navbar>
       <Container fluid>
-        <Navbar.Brand href="/dynamics">Отчеты</Navbar.Brand>
+        <Row className="mb-3">
+          <Col>
+            <Filter/>
+          </Col> 
+        </Row>
+        
+          <Row className="mb-3">
+              <Col>
+                <DynamicSale />
+              </Col>
+          </Row>
       </Container>
-    </Navbar>
-    <Container fluid>
-      <Row className="mb-3">
-        <Col>
-          <Filter/>
-        </Col>
-      </Row>
-      <DynamicsPageContent />
-    </Container>
+    </Login>
   </>
-}
-
-
-const DynamicsPageContent = () => {
-  return (
-    <Row className="mb-3">
-        <Col>
-          <DynamicSale />
-        </Col>
-    </Row>
-  )
 }
 
 export default DynamicsPage
