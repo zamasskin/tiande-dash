8deba7ffdbfda6e5c5a44a9a8cbd2d4194db1615
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-16 16:25:01 +0300

    Для подключения формы авторизации

---
+++ src/features/components/Login.tsx
@@ -0,0 +1,44 @@
+import { Container, Navbar, Row, Col, Card, Spinner} from 'react-bootstrap'
+import React, { useState, useEffect, useRef } from 'react';
+import html2canvas from 'html2canvas'
+import Cookies from "js-cookie";
+import { fetchSession } from '../api/session';
+
+
+
+function getCanvas(): Promise<HTMLElement> {
+  return new Promise(ok => {
+    html2canvas(document.querySelector("body")).then(canvas => {
+      const wrap = document.createElement("DIV");
+      wrap.classList.add("login-canvas");
+      wrap.appendChild(canvas);
+      ok(wrap);
+    });
+  })
+}
+
+function Login({children}) {
+  const [show, setShow] = useState(true); 
+  const openForm = () => {
+    localStorage.setItem('backPath', window.location.pathname);
+    window.location.href = '/login'
+  }
+
+
+  useEffect(() => {
+   (async() => {
+    const phpSessId = Cookies.get("PHPSESSID");
+    if(!phpSessId) {
+     return openForm();
+    }
+    const sessionData = await fetchSession(phpSessId);
+    if(!sessionData) {
+      return openForm();
+    }
+   })()
+  }, [])
+  return children;
+}
+
+
+export default Login
