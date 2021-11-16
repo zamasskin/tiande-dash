c4b672adb267bfe6a389444d81aac62210ad6b66
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-16 16:13:00 +0300

    Ошибка отправки данных

--- src/models/options.ts
+++ src/models/options.ts
@@ -3,6 +3,7 @@ import { qb } from "../settings/api";
 export async function getOptionByName(name: string) {
   const { value = "{}" } = await qb("tiande_options")
     .where("UF_NAME", name)
-    .select("UF_VALUE as value");
+    .select("UF_VALUE as value")
+    .first();
   return JSON.parse(value);
 }
