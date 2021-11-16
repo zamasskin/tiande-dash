4c8b743c4c5602e58324dfbd66af6f7427690f02
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-16 13:04:58 +0300

    Сделал чтобы при перетаскивании подгружались данные

--- src/features/dynamics/DynamicSale.tsx
+++ src/features/dynamics/DynamicSale.tsx
@@ -2,14 +2,14 @@ import _ from 'lodash'
 import dynamic from 'next/dynamic'
 import { useState, useEffect } from 'react';
 import { Col, Card, Table} from 'react-bootstrap'
-import * as csDictionary from 'plotly.js/lib/locales/ru.js';
 
-import { useAppSelector } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchDynamicSaleList } from '../api/dynamics';
-import { selectFilterIndicator } from './filterSlice';
+import { selectFilterIndicator,  setPeriodStart, setPeriodEnd } from './filterSlice';
 import Plot from '../components/Plotly/Plot'
 
 function DynamicSale() {
+  const dispatch = useAppDispatch()
   const filter = useAppSelector(selectFilterIndicator);
   const [data, setData] = useState([]);
   const [preloader, setPreloader] = useState(false);
@@ -20,6 +20,14 @@ function DynamicSale() {
     })().finally(() => setPreloader(false));
   }, [filter])
 
+  const  onRelayout = (data) => {
+    if(!data['xaxis.range[0]'] || !data['xaxis.range[1]']) {
+      return;
+    }
+    dispatch(setPeriodStart(new Date(data['xaxis.range[0]']).getTime()))
+    dispatch(setPeriodEnd(new Date(data['xaxis.range[1]']).getTime()))
+  }
+
   return (
     <Col>
       <Card className={preloader ? "shadow-sm preloader" : "shadow-sm"}>
@@ -28,6 +36,7 @@ function DynamicSale() {
             data={data} 
             layout={{title: 'Динамика продаж', autosize: true}}
             style={{width: "100%", height: "100%"}}
+            onRelayout={onRelayout}
           ></Plot>
         </Card.Body>
       </Card>
