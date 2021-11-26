6d211e1fce4c1bcf359012f168ba47bd05fc20f0
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-26 11:42:11 +0300

    рекомендация

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
  */
 
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
